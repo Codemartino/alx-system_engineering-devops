2730a600fe7fd22850fe27b4f7e9c805717520cf
Codemartino <akudugumartin5@gmail.com>
2023-09-22 10:34:39 -0700

    Fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,7 @@
-# Create a file
-
-file { '/tmp/School':
-  ensure  => 'present',
-  replace => 'no',
-  group   => 'www-data',
-  owner   => 'www-data',
-  mode    => '0744',
-  content => "I love Puppet\n",
-}
+# Creating A file in tmp folder named school
+file { '/tmp/school':
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet', }
